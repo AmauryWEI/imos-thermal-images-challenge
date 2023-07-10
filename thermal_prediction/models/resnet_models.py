1fe6faaac5bd6cd92437701c668707731d135479
Amaury WEI <amaury.wei@gmail.com>
2023-07-10 21:08:07 +0200

    model: create resnet50 metadata mlp model
    - Pre-trained ResNet50 for images
    - Multi-Layer Perceptron (MLP) for metadata
    - Concatenate ResNet50 out with MLP out

--- thermal_prediction/models/resnet_models.py
+++ thermal_prediction/models/resnet_models.py
@@ -2,7 +2,7 @@
 # Date:         2023/07/09
 # Description:  Models based on ResNet pre-trained architectures
 
-from torch import Tensor, relu, cat
+from torch import Tensor, tanh, cat
 from torch.nn import Module, Linear, Identity
 from torchvision.models import resnet50, ResNet50_Weights
 
@@ -82,6 +82,68 @@ class ResNet50_RgbMetadata(Module):
 
         # Network Out with ResNet Out and metadata
         out = cat((resnet_out, metadata), 1)
+        # No activation function to output a continuous value
+        out = self.__fc(out)
+
+        return out
+
+
+class ResNet50_RgbMetadataMlp(Module):
+    """
+    Input:  Image (cols: 384 ; rows: 288) (Tensor 288 x 384) + Metadata (Tensor 9 x 1)
+    Output: Temperature (float)
+    """
+
+    def __init__(self):
+        super(ResNet50_RgbMetadataMlp, self).__init__()
+
+        ################################
+        # ResNet50 Network Configuration
+        ################################
+
+        # Import a pre-trained ResNet50 network
+        self.__resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+
+        # Do not fine-tune the ResNet50 weights
+        for param in self.__resnet.parameters():
+            param.requires_grad = False
+
+        # FineTune only the last ResNet layer
+        for param in self.__resnet.layer4.parameters():
+            param.requires_grad = True
+
+        # Image pre-processing layer (resize, center cropping, ImageNet normalization)
+        self.__resnet_preprocess = ResNet50_Weights.DEFAULT.transforms(antialias=False)
+
+        # Replace the default ResNet Fully-Connected layer by Identity (to get the raw 2048 values)
+        self.__resnet.fc = Identity()
+
+        #####################################
+        # Multi-Layer Perceptron for Metadata
+        #####################################
+
+        # Number of features to extract from the metadata
+        self.__mlp_out_size = 9
+        # Input metadata size = 9
+        self.__mlp = Linear(9, self.__mlp_out_size)
+
+        ###########################################
+        # Network Output with ResNet features + MLP
+        ###########################################
+
+        # Output of ResNet50 (= 2048) + Output of MLP
+        self.__fc = Linear(2048 + self.__mlp_out_size, 1)
+
+    def forward(self, image: Tensor, metadata: Tensor):
+        # ResNet image processing
+        image = self.__resnet_preprocess(image)
+        resnet_out = self.__resnet.forward(image)
+
+        # MLP metadata processing
+        mlp_out = tanh(self.__mlp(metadata))
+
+        # Network Out with ResNet Out and metadata
+        out = cat((resnet_out, mlp_out), 1)
         # No activation function to output a continuous value
         out = self.__fc(out)
 
