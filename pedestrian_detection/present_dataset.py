098785cefd19d6ab41e8cfa7fd8f660f0c49c832
Amaury WEI <amaury.wei@gmail.com>
2023-07-12 18:41:56 +0200

    data: parse data folders from cli arguments

--- pedestrian_detection/present_dataset.py
+++ pedestrian_detection/present_dataset.py
@@ -5,12 +5,44 @@
 # Description:  Top-level script to visualize and analyze the Pedestrian Detection dataset
 
 import argparse
+from os import path
 
 # Define the arguments/options of the script
 parser = argparse.ArgumentParser()
 
+# Mandatory arguments
+parser.add_argument(
+    "data_folders",
+    metavar="data_folders",
+    nargs="+",
+    help="Folders containing images and labels",
+    default=[],
+)
+
+# Optional arguments
+parser.add_argument(
+    "-q",
+    "--quiet",
+    help="Run the script with minimal log output",
+    action="store_true",
+    default=0,
+)
+
 
 def main(args: argparse.Namespace) -> int:
+    if not args.quiet:
+        print(f"Data folders ({len(args.data_folders)}): {args.data_folders}")
+
+    # Make sure each folder exists
+    data_folders_abs_path = [path.abspath(f) for f in args.data_folders]
+    for folder in data_folders_abs_path:
+        if not path.exists(folder):
+            print(f"ERROR: {folder} does not exist.")
+            return 1
+        if not path.isdir(folder):
+            print(f"ERROR: {folder} is not a folder.")
+            return 1
+
     return 0
 
 
