b0d10e8c2fe82992e7a861070dc7f718e19fb439
Amaury WEI <amaury.wei@gmail.com>
2023-07-12 18:21:59 +0200

    data: create script template for present_dataset

---
+++ pedestrian_detection/present_dataset.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# File:         present_dataset.py
+# Date:         2023/07/12
+# Description:  Top-level script to visualize and analyze the Pedestrian Detection dataset
+
+import argparse
+
+# Define the arguments/options of the script
+parser = argparse.ArgumentParser()
+
+
+def main(args: argparse.Namespace) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    ret = main(args)
+    exit(ret)
