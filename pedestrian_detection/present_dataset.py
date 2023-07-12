aa8fff93d4236bb922a8e92571db17aa72e9a148
Amaury WEI <amaury.wei@gmail.com>
2023-07-12 22:24:26 +0200

    plot: show 9 random images with bounding boxes

--- pedestrian_detection/present_dataset.py
+++ pedestrian_detection/present_dataset.py
@@ -8,6 +8,12 @@ import argparse
 from os import path
 import sys
 
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.v2 import functional as F
+
 sys.path.append("./loaders/")
 from pedestrian_dataset import PedestrianDataset
 
@@ -33,6 +39,46 @@ parser.add_argument(
 )
 
 
+def show_random_images(dataset: PedestrianDataset) -> None:
+    """
+    Show multiple random images of the dataset
+
+    Parameters
+    ----------
+    dataset : PedestrianDataset
+        Dataset containing the images and bounding boxes
+    """
+    ROWS_COUNT = 3
+    COLS_COUNT = 3
+    images_fig, axes = plt.subplots(nrows=ROWS_COUNT, ncols=COLS_COUNT, figsize=(14, 8))
+    images_fig.canvas.manager.set_window_title("Dataset Images")
+
+    for row_idx in range(ROWS_COUNT):
+        for col_idx in range(COLS_COUNT):
+            image_idx = np.random.randint(0, len(dataset))
+
+            tensor_image, target = dataset[image_idx]
+            tensor_image = F.convert_dtype(tensor_image, torch.uint8)
+
+            # If objects are in the image, draw their bounding boxes
+            if len(target["boxes"]) > 0:
+                tensor_image = draw_bounding_boxes(
+                    tensor_image,
+                    target["boxes"],
+                    colors="yellow",
+                    width=3,
+                )
+
+            # Permute the channels for plotting (in: 3 x H x W ; out: H x W x 3)
+            axes[row_idx, col_idx].imshow(tensor_image.permute(1, 2, 0))
+            # Set image title and turn-off tick labels
+            axes[row_idx, col_idx].set_title(f"Idx: {image_idx}")
+            axes[row_idx, col_idx].set_yticklabels([])
+            axes[row_idx, col_idx].set_xticklabels([])
+
+    images_fig.tight_layout()
+
+
 def main(args: argparse.Namespace) -> int:
     if not args.quiet:
         print(f"Data folders ({len(args.data_folders)}): {args.data_folders}")
@@ -49,6 +95,9 @@ def main(args: argparse.Namespace) -> int:
 
     # Create a dataset from the folders
     dataset = PedestrianDataset(data_folders_abs_path, quiet=args.quiet)
+
+    show_random_images(dataset)
+    plt.show()
 
     return 0
 
