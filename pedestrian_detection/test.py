d66298c57dc972e04da72ae359678c8bc0bd1671
Amaury WEI <amaury.wei@gmail.com>
2023-07-13 19:20:38 +0200

    test: parse multiple data folders and load dataset

--- pedestrian_detection/test.py
+++ pedestrian_detection/test.py
@@ -6,17 +6,27 @@
 
 import argparse
 from os import path
+import sys
+
+import torch
+from torch.nn import Module
+from torch.utils.data import Dataset
+
+sys.path.append("./loaders/")
+from pedestrian_dataset import PedestrianDataset
 
 # Define the arguments/options of the script
 parser = argparse.ArgumentParser()
 
 # Mandatory arguments
 parser.add_argument(
-    "data_folder",
-    metavar="testing_data_folder",
-    help="Unique folder containing images and labels for testing",
-    type=str,
+    "data_folders",
+    metavar="data_folders",
+    nargs="+",
+    help="Folders containing images and labels for testing",
+    default=[],
 )
+
 
 # Optional arguments
 parser.add_argument(
@@ -29,15 +39,28 @@ parser.add_argument(
 
 
 def main(args: argparse.Namespace) -> int:
-    test_data_dir_abs_path = path.abspath(args.data_folder)
+    device = torch.device("cpu")
+    if torch.backends.mps.is_available():
+        # device = torch.device("mps") # Disabled because of an issue on macOS
+        device = torch.device("cpu")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
 
-    # Make sure the test data folder exists
-    if not path.exists(test_data_dir_abs_path):
-        print(f"ERROR: {test_data_dir_abs_path} does not exist.")
-        return 1
-    if not path.isdir(test_data_dir_abs_path):
-        print(f"ERROR: {test_data_dir_abs_path} is not a folder.")
-        return 1
+    if not args.quiet:
+        print(f"Data folders ({len(args.data_folders)}): {args.data_folders}")
+
+    # Make sure each folder exists
+    data_folders_abs_path = [path.abspath(f) for f in args.data_folders]
+    for folder in data_folders_abs_path:
+        if not path.exists(folder):
+            print(f"ERROR: {folder} does not exist.")
+            return 1
+        if not path.isdir(folder):
+            print(f"ERROR: {folder} is not a folder.")
+            return 1
+
+    # Load the dataset
+    dataset = PedestrianDataset(data_folders_abs_path, quiet=args.quiet)
 
     return 0
 
