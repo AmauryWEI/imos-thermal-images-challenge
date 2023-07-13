c8a731a87739012e1213088a7057df43e6cc6973
Amaury WEI <amaury.wei@gmail.com>
2023-07-13 19:11:25 +0200

    test: add skeleton for top-level testing script

---
+++ pedestrian_detection/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# File:         test.py
+# Date:         2023/07/13
+# Description:  Top-level script to test a Neural Network for the Challenge #2
+
+import argparse
+
+# Define the arguments/options of the script
+parser = argparse.ArgumentParser()
+
+
+def main(args: argparse.Namespace) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    ret = main(args)
+    exit(ret)
