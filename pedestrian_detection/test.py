7f3c2eae0a48b3cbcb01422f72006298aad7b008
Amaury WEI <amaury.wei@gmail.com>
2023-07-13 19:15:36 +0200

    test: parse testing data folder from arguments

--- pedestrian_detection/test.py
+++ pedestrian_detection/test.py
@@ -5,12 +5,40 @@
 # Description:  Top-level script to test a Neural Network for the Challenge #2
 
 import argparse
+from os import path
 
 # Define the arguments/options of the script
 parser = argparse.ArgumentParser()
 
+# Mandatory arguments
+parser.add_argument(
+    "data_folder",
+    metavar="testing_data_folder",
+    help="Unique folder containing images and labels for testing",
+    type=str,
+)
+
+# Optional arguments
+parser.add_argument(
+    "-q",
+    "--quiet",
+    help="Run the script with minimal log output",
+    action="store_true",
+    default=0,
+)
+
 
 def main(args: argparse.Namespace) -> int:
+    test_data_dir_abs_path = path.abspath(args.data_folder)
+
+    # Make sure the test data folder exists
+    if not path.exists(test_data_dir_abs_path):
+        print(f"ERROR: {test_data_dir_abs_path} does not exist.")
+        return 1
+    if not path.isdir(test_data_dir_abs_path):
+        print(f"ERROR: {test_data_dir_abs_path} is not a folder.")
+        return 1
+
     return 0
 
 
